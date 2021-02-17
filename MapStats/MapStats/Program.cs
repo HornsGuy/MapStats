335ca4522b518d1e65f2009e2cded2248b5fc6a2
Tanner Evans <thathornsguy@gmail.com>
2021-02-17 09:34:06 -0700

    Add C# project

---
+++ MapStats/MapStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
